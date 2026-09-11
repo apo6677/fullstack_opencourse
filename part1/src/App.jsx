b9093dfe48af0f8b9fc5b7b3eeedb96790951364
apo6677 <apollotaouxis@gmail.com>
2026-09-11 13:16:45 +0300

    part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,23 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Alice" age={20} />
+      <Hello name="Bob" age={25} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App
