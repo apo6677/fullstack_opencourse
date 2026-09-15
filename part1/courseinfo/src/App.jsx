8627cff6e0f6f4241116d1f60017ed92942fe7f2
apo6677 <apollotaouxis@gmail.com>
2026-09-15 16:24:50 +0300

    1.6: unicafe step 1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,62 +1,23 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-    </div>
-  )
-}
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.info} {props.exercises}
-      </p>
-    </div>
-  )
-}
-const Content = (props) => {
-  return (
-    <div>
-      <Part info={props.name1} exercises={props.exercises_1} />
-      <Part info={props.name2} exercises={props.exercises_2} />
-      <Part info={props.name3} exercises={props.exercises_3} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.exercises_1 + props.exercises_2 + props.exercises_3}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [value, setValue] = useState(10)
+  
 
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
   return (
     <div>
-      <Header name={course.name} />
-      <Content name1={course.parts[0].name} exercises_1={course.parts[0].exercises} name2={course.parts[1].name} exercises_2={course.parts[1].exercises} name3={course.parts[2].name} exercises_3={course.parts[2].exercises} />
-      <Total exercises_1={course.parts[0].exercises} exercises_2={course.parts[1].exercises} exercises_3={course.parts[2].exercises} />
+      {value}
+
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
+
 export default App
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return ( 
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={increaseGood} text="good" />
+      <Button onClick={increaseNeutral} text="neutral" />
+      <Button onClick={increaseBad} text="bad" />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
