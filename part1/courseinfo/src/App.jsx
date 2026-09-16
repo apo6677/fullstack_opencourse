86911fce06dd25fba9420c626326c423e31d2586
apo6677 <apollotaouxis@gmail.com>
2026-09-16 17:24:11 +0300

    2.5: Separate module step 10

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,42 +1,5 @@
-const Header = ({ course_name }) => {
-  return <h1>{course_name}</h1>
-}
+import Course from './components/course'
 
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => {
-    console.log('what is happening here', sum, part.exercises)
-    return sum + part.exercises
-  }, 0)
-  return <h3>Total number of exercises: {totalExercises}</h3>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course_name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
 
 const App = () => {
   const courses = [
