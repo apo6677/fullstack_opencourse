43a937076ecbdfb4cd781cc3c28c6130220bb2f5
apo6677 <apollotaouxis@gmail.com>
2026-09-18 18:46:37 +0300

    2.9*: The Phonebook Step 4

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Arto Hellas', number: '040-123456', id: 1, important: true },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2, important: true },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3, important: true },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4, important: true }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
+  const personsToShow = persons.filter(person => person.important)
+  console.log(persons.map(person => person.important))
+
+  const handleSearch = (event) => {
+    const searchValue = event.target.value.toLowerCase()
+    setPersons(
+      persons.map(person => ({
+        ...person,
+        important: person.name.toLowerCase().includes(searchValue)
+      }))
+    )
+  }
+
   const checkSubmit = () => {
-    if (persons.map(person => person.name).includes(newName) || persons.map(person => person.number).includes(newNumber)){
+    if (persons.map(person => person.name).includes(newName) || persons.map(person => person.number).includes(newNumber)) {
       return true
     }
     else {
@@ -21,6 +37,7 @@ const App = () => {
     const nameObject = {
       name: newName,
       number: newNumber,
+      important: true,
       id: String(persons.length + 1),
     }
 
@@ -45,6 +62,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <input
+        type="search"
+        placeholder="Type here to filter the list"
+        onInput={handleSearch} />
       <form onSubmit={handleSubmit}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
@@ -55,7 +76,7 @@ const App = () => {
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {personsToShow.map(person =>
           <li key={person.id}>{person.name} {person.number}</li>
         )}
       </ul>
